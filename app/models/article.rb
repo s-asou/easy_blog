12a1077e3d2443220471c8e3cb96aa6d9b301454
Shingo Aso <s-aso@zerotoone.co.jp>
2019-08-09 12:03:33 +0900

    5.10 add validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
+# ブログ記事モデル
+#
+# @attr [Integer] id    ブログ記事ID
+# @attr [String]  title ブログ記事タイトル
+# @attr [Text]    text  ブログ記事内容
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
 end
