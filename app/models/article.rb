5affc6eb360348bb0146278653c152bf78dd562e
Shingo Aso <s-aso@zerotoone.co.jp>
2019-08-14 14:28:09 +0900

    8.1 delete object associated with

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,6 @@
 # @attr [String]  title title
 # @attr [Text]    text  content
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end
